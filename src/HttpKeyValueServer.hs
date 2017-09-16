6c30a4dc202e434e107bdd1867aab7f6ea2c4285
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2017-09-16 23:25:37 +0200

    Read POST content

--- src/HttpKeyValueServer.hs
+++ src/HttpKeyValueServer.hs
@@ -1,6 +1,19 @@
 import Control.Monad (MonadPlus, msum)
-import Happstack.Server (FilterMonad, Response, Method(GET, POST), ServerMonad, method, nullConf, simpleHTTP, toResponse, ok, dir, path)
+import Happstack.Server (BodyPolicy, FilterMonad, Response, Method(GET, POST), ServerMonad,
+                         decodeBody, defaultBodyPolicy, look, method, methodM, nullConf, simpleHTTP, toResponse, ok, dir, path)
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum [ dir "set" $ (method POST >> (path $ \p -> ok ("You want to POST set key: " ++ p ++ "\n")))
-                                  , dir "get" $ (method GET  >> (path $ \p -> ok ("You want to get key: " ++ p ++ "\n"))) ]
+main = simpleHTTP nullConf $ do
+    decodeBody decodePolicy
+    msum [ get, set ]
+
+decodePolicy :: BodyPolicy
+decodePolicy = (defaultBodyPolicy "/tmp/" 0 1000 1000)
+
+set = dir "set" $ do
+    method POST
+    path $ \p -> do
+        value <- look "value"
+        ok ("You want to set key: " ++ p ++ ", with value: " ++ value ++ "\n")
+
+get = dir "get" $ (method GET  >> (path $ \p -> ok ("You want to get key: " ++ p ++ "\n")))
